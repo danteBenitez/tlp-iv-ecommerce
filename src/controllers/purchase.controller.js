a488829f25ced79e31b2739d0927a82e176cc2dc
danteBenitez <benitezdante123@gmail.com>
2024-08-15 20:37:17 -0300

    feat: Ruta de compra de productos

---
+++ src/controllers/purchase.controller.js
@@ -0,0 +1,51 @@
+import { ProductNotFoundError } from "../services/product.service.js";
+import { OutOfStockError, purchaseService } from "../services/purchase.service.js";
+
+export class PurchaseController {
+  /** @type {typeof purchaseService} */
+  #purchaseService;
+
+  constructor(purchaseService) {
+    this.#purchaseService = purchaseService;
+  }
+
+
+  /**
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   */
+  async buyProductsInBulk(req, res) {
+    const user = req.user;
+    const { products, payment_method } = req.body;
+
+    try {
+      const purchase = await this.#purchaseService.buyInBulk(
+        {
+          payment_method,
+        },
+        products,
+        user
+      );
+
+      res.status(200).json({
+        ...purchase,
+        message: "Compra realizada con éxito",
+      });
+    } catch (err) {
+      if (err instanceof ProductNotFoundError) {
+        return res.status(404).json({
+          message: err.message,
+        });
+      }
+      if (err instanceof OutOfStockError) {
+        return res.status(400).json({
+          message: err.message,
+        });
+      }
+      console.error("Error al realizar compra: ", err);
+      return res.status(500).json({
+        message: "Error interno del servidor",
+      });
+    }
+  }
+}
--- src/routes/purchase.routes.js
+++ src/routes/purchase.routes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
+import { ROLES } from "../consts/roles.js";
+import { PurchaseController } from "../controllers/purchase.controller.js";
+import { roleMiddleware } from "../middleware/role.middleware.js";
+import { purchaseService } from "../services/purchase.service.js";
 import { validationMiddlewareFor } from "../utils/validation-middleware.js";
 import { makePurchaseSchema } from "../validations/purchase.validation.js";
 
 const router = Router();
+const controller = new PurchaseController(purchaseService);
 
-router.post("/", [validationMiddlewareFor(makePurchaseSchema)], (req, res) =>
-  res.json({ msg: "Hello, world" })
+router.post(
+  "/",
+  [...roleMiddleware(ROLES.BUYER), validationMiddlewareFor(makePurchaseSchema)],
+  (req, res) => controller.buyProductsInBulk(req, res)
 );
 
 export default router;
