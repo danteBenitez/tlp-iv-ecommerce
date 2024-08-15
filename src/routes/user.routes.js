ea8625332990e7ef4b4f02743785f372c434a3a7
danteBenitez <benitezdante123@gmail.com>
2024-08-15 12:10:17 -0300

    fix: Llamada de controladores incorrecta

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,26 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller.js";
 import { usersService } from "../services/user.service.js";
+import { validationMiddlewareFor } from "../utils/validation-middleware.js";
+import {
+  signInUserSchema,
+  signUpUserSchema,
+} from "../validations/user.validation.js";
 
 const router = Router();
 
 const controller = new UserController(usersService);
 
-router.post("/auth/sign-in", controller.signIn);
-router.post("/auth/sign-up", controller.signUp);
+router.post(
+  "/auth/sign-in",
+  [validationMiddlewareFor(signInUserSchema)],
+  (req, res) => controller.signIn(req, res)
+);
 
-export default router; 
+router.post(
+  "/auth/sign-up",
+  [validationMiddlewareFor(signUpUserSchema)],
+  (req, res) => controller.signUp(req, res)
+);
+
+export default router;
