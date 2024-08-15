ec39fcc9a78cd8b8eb4f9dcec54c8cc095ccbbaa
danteBenitez <benitezdante123@gmail.com>
2024-08-15 12:29:29 -0300

    feat: Validar datos de registro e inicio de sesión

--- src/consts/roles.js
+++ src/consts/roles.js
@@ -3,3 +3,6 @@ export const ROLES = {
     SELLER: "seller",
     BUYER: "buyer"
 }
+
+/** Roles permitidos a ser asignados a usuarios no administradores */
+export const ALLOWED_ROLES = [ROLES.SELLER, ROLES.BUYER];
---
+++ src/validations/user.validation.js
@@ -0,0 +1,57 @@
+import { body } from "express-validator";
+import { ALLOWED_ROLES } from "../consts/roles.js";
+
+export const signInUserSchema = [
+  body("username")
+    .notEmpty()
+    .withMessage("El `username` es requerido")
+    .isString()
+    .withMessage("`username` debe ser un string"),
+  body("password")
+    .notEmpty()
+    .withMessage("La `password` es requerida")
+    .isString()
+    .withMessage("`password` debe ser un string"),
+];
+
+const formatter = new Intl.ListFormat("es", {
+  type: "disjunction",
+});
+const allowedRolesMessage = formatter.format(ALLOWED_ROLES);
+
+export const signUpUserSchema = [
+  body("username")
+    .notEmpty()
+    .withMessage("El `username` es requerido")
+    .isString()
+    .withMessage("`username` debe ser un string"),
+  body("password")
+    .notEmpty()
+    .withMessage("La `password` es requerida")
+    .isString()
+    .withMessage("`password` debe ser un string")
+    .isStrongPassword({
+      minLength: 8,
+      minSymbols: 0,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+    })
+    .withMessage(
+      "La `password` no es segura. Debe contener mínimo 8 caracteres, una mayúscula, una minúscula y un número"
+    ),
+  body("email")
+    .notEmpty()
+    .withMessage("El `email` es requerido")
+    .isString()
+    .withMessage("`email` debe ser un string")
+    .isEmail()
+    .withMessage("`email` inválido"),
+  body("role")
+    .notEmpty()
+    .withMessage("El `role` es requerido")
+    .isString()
+    .withMessage("`role` debe ser un string")
+    .isIn(ALLOWED_ROLES)
+    .withMessage(`\`role\` debe ser uno de ${allowedRolesMessage}`),
+];
