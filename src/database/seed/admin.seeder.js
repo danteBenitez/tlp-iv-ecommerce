0401d9c566df5d4276610f98c46aaf094206efcb
danteBenitez <benitezdante123@gmail.com>
2024-08-18 01:03:06 -0300

    fix: Cambiar lógica de seed de usuario administrador

--- src/database/seed/admin.seeder.js
+++ src/database/seed/admin.seeder.js
@@ -1,30 +1,39 @@
 import { ROLES } from "../../consts/roles.js";
 import { Role } from "../../models/role.model.js";
+import { UserRole } from "../../models/user-roles.model.js";
 import { User } from "../../models/user.model.js";
 import { encryptionService } from "../../services/encryption.service.js";
 
 export async function seedAdmin() {
-    const adminRole = await Role.findOne({
-        where: {
-            name: ROLES.ADMIN
-        }
+  const adminRole = await Role.findOne({
+    where: {
+      name: ROLES.ADMIN,
+    },
+  });
+
+  if (!adminRole) {
+    throw new Error(
+      "Rol de administrador faltante. Sincronice la base de datos"
+    );
+  }
+
+  const adminUser = await UserRole.findOne({
+    where: {
+      role_id: adminRole.role_id,
+    },
+  });
+
+  if (!adminUser) {
+    // Si no hay usuario con el rol de administrador,
+    // creamos uno génerico.
+    const user = await User.create({
+      username: "admin",
+      email: "admin@example.com",
+      password: await encryptionService.encrypt("admin"),
     });
+    // Y le añadimos el rol correspondiente.
+    user.addRole(adminRole);
+  }
 
-    if (!adminRole) {
-        throw new Error("Rol de administrador faltante. Sincronice la base de datos");
-    }
-
-    const adminUser = await User.findOrCreate({
-        where: {
-            role_id: adminRole.role_id
-        },
-        defaults: {
-            username: "admin",
-            email: "admin@example.com",
-            password: await encryptionService.encrypt("admin"),
-            role_id: 1
-        }
-    });
-
-    return adminUser;
+  return adminUser;
 }
